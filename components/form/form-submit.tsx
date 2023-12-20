c5be39ebd179de56c7060acc592ad588fb789d52
YarinKeren <jarin4378@gmail.com>
2023-12-20 13:29:08 +0100

    field validation

---
+++ components/form/form-submit.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useFormStatus } from 'react-dom'
+
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+
+interface FormSubmitProps {
+  children: React.ReactNode
+  disabled?: boolean
+  className?: string
+  variant?: 'default' | 'destructive' | 'secondary' | 'outline' | 'ghost' | 'link' | 'primary'
+}
+
+export const FormSubmit = ({ children, disabled, className, variant }: FormSubmitProps) => {
+  const { pending } = useFormStatus()
+
+  return (
+    <Button disabled={pending || disabled} type='submit' variant={variant} size='sm' className={cn(className)}>
+      {children}
+    </Button>
+  )
+}
