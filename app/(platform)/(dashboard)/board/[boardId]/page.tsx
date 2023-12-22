03cf4057f803fb56caecf5fcadfc3e4a2ec16640
YarinKeren <jarin4378@gmail.com>
2023-12-22 15:33:18 +0100

    added lists rendering, list-title editing

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,3 +1,42 @@
-export default function Page() {
-  return <div>BoardId</div>
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import ListContainer from './_components/list-container'
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string
+  }
+}
+
+export default async function Page({ params }: BoardIdPageProps) {
+  const { orgId } = auth()
+  const { boardId } = params
+
+  if (!orgId) redirect('/select-org')
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: 'asc',
+        },
+      },
+    },
+    orderBy: {
+      order: 'asc',
+    },
+  })
+
+  return (
+    <div className='p-4 h-full overflow-x-auto'>
+      <ListContainer boardId={boardId} data={lists} />
+    </div>
+  )
 }
